43ec3642616e828f7f01d433fa7c39dc0f76f7bb
Stu Franks <stu@stuts.uk>
2025-02-22 14:37:28 +0000

    Use margin of victory to skew elo

--- app/models/rater.rb
+++ app/models/rater.rb
@@ -19,7 +19,8 @@ module Rater
       end
     end
 
-    def update_ratings game, teams
+    def update_ratings game, result
+      teams = result.teams
       winning_teams = teams.select{|team| team.rank == Team::FIRST_PLACE_RANK}
 
       if winning_teams.size > 1
@@ -42,7 +43,41 @@ module Rater
         first_elo = to_elo(first_rating)
         second_elo = to_elo(second_rating)
 
-        first_elo.wins_from(second_elo)
+        match = first_elo.versus(second_elo)
+        # Skew value based on margin of victory
+        ## The 'result' can be from 0 to 1:
+        ##   1 - player one wins (what happens here)
+        ##   0.5 - it is a draw
+        ## Knowing this we can skew the result towards 0.5 in situations
+        ## where the margin of victory is smaller.
+        ##
+        ## Based on how this system works (first_elo is always the winner) we
+        ## can assume that 'for' is larger than 'against' and use that to generate
+        ## a value with which the result can be skewed
+        ##
+        ## Foosball
+        ## 10-0 = 10 should become 1, a perfect victory
+        ## 9-1 = 8
+        ## 6-4 = 2 should be closer to 0.5, a narrow victory
+        ##
+        ## Ping Pong
+        ## 11-0 = 11 should become 1, a perfect victory
+        ## 11-5 = 6
+        ## 11-9 = 2 should be closer to 0.5, a narrow victory
+        ##
+        ## Use 'for' as 100% and get the scale of points conceded 'against'
+        ## and then divide by 2 so that the value will never be 0.5 or above
+        ## because if it divided by 2 and became 0.5 then it was a draw!
+        ## result = 1 - ((against / for) / 2)
+        ##   10-0 = 1 - ((0 / 10)  / 2)= 1 - 0 = 1, a perfect victory
+        ##   6-4 = 1 - ((4 / 6) / 2) = 1 - 0.3 = 0.7, a narrow victory
+        ##
+        ## Testing with a blank database and first match between "one" and "two"
+        ##   "one" defeats "two" 10-0: one elo 1012, two elo 987
+        ##   "one" defeats "two" 6-4: one elo 1004, two elo 995
+        skew = (result.against.to_f / result.for ).to_f / 2
+        result = 1 - skew
+        match.result = result
       end
 
       _update_rating_from_elo(first_rating, first_elo)
