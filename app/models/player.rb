e0a9e96489483e3f24f80ce347f380232669e28a
Stu Franks <stu@stuts.uk>
2025-03-06 15:32:38 +0000

    Add best win/lose/unbeaten streak tracking

--- app/models/player.rb
+++ app/models/player.rb
@@ -52,6 +52,16 @@ class Player < ApplicationRecord
     results.where(game_id: game, teams: { winner: true }).to_a.count { |r| !r.tie? }
   end
 
+  def total_wins_on_for(game)
+    # Number of wins on the "For" side
+    results.where(game_id: game, teams: { winner: true }).to_a.count { |r| r.side == 'for' }
+  end
+
+  def total_wins_on_against(game)
+    # Number of wins on the "Against" side
+    results.where(game_id: game, teams: { winner: true }).to_a.count { |r| r.side == 'against' }
+  end
+
   def wins(game, opponent)
     results.where(game_id: game, teams: { winner: true }).against(opponent).to_a.count { |r| !r.tie? }
   end
@@ -62,5 +72,112 @@ class Player < ApplicationRecord
 
   def losses(game, opponent)
     results.where(game_id: game, teams: { winner: false }).against(opponent).to_a.count { |r| !r.tie? }
+  end
+
+  def get_all_streaks(game)
+    streaks = []
+    currentStreak = Streak.new()
+    currentStreakUnbeaten = Streak.new()
+    currentStreakUnbeaten.unbeaten = true
+
+    results = self.results.where(game_id: game)
+
+    for result in results
+      # Identify whether this match was a win, loss or draw 
+      won_game = result.winners.include?(self) && !result.tie?
+      lost_game = !result.winners.include?(self) && !result.tie?
+      tie_game = result.tie?
+
+      # Continue win streak if we're on one
+      if (won_game and currentStreak.win) 
+        # Add to win streak
+        currentStreak.resultTimes.append(result.created_at)
+        # Add to current unbeaten streak
+        currentStreakUnbeaten.resultTimes.append(result.created_at)
+      # Continue loss streak if we're on one
+      elsif (lost_game and not currentStreak.win)
+        # Add to loss streak 
+        currentStreak.resultTimes.append(result.created_at)
+      # Add to unbeaten run if we're on one
+      elsif tie_game and currentStreak.win 
+        # Add to current unbeaten streak
+        currentStreakUnbeaten.resultTimes.append(result.created_at)
+      end
+
+      if (won_game and not currentStreak.win) or (lost_game and currentStreak.win) or (tie_game and not currentStreak.win)
+        puts "Streak over: #{tie_game && 'tie' || (won_game && 'win' || 'lose')} during a #{currentStreak.win && 'win' || 'lose'} streak"
+        # end of streak
+        if currentStreak.count >= 1
+          # Streak over and more than 1 match so adding to past streaks
+          streaks.append(currentStreak)
+        end
+        # Start new streak
+        currentStreak = Streak.new()
+        if lost_game
+          # Save and reset unbeaten streak
+          streaks.append(currentStreakUnbeaten)
+          currentStreakUnbeaten = Streak.new()
+        end
+        if won_game or lost_game
+          # Starting new #{ won_game && 'win' || 'lose'} streak
+          currentStreak.resultTimes.append(result.created_at)
+        end
+        currentStreak.win = won_game
+      end
+    end
+    return {'past': streaks, 'current': currentStreak}
+  end
+
+  def get_streaks(game)
+    streaks = get_all_streaks(game)
+    winStreaks = streaks[:past].select {|s| s.win && !s.unbeaten }.sort_by {|s| s.resultTimes.size }.reverse
+    loseStreaks = streaks[:past].select {|s| !s.win }.sort_by {|s| s.resultTimes.size }.reverse
+    unbeatenStreaks = streaks[:past].select {|s| s.win && s.unbeaten }.sort_by {|s| s.resultTimes.size }.reverse
+    currentStreakType = streaks[:current].unbeaten && "unbeaten" || (streaks[:current].win &&  "win(s)" || "loss(es)")
+    return {
+      'win': winStreaks.size > 0 && winStreaks[0] || Streak.new(),
+      'lose': loseStreaks.size > 0 && loseStreaks[0] || Streak.new(),
+      'unbeaten': unbeatenStreaks.size > 0 && unbeatenStreaks[0] || Streak.new(),
+      'current': streaks[:current],
+      'currentType': currentStreakType
+    }
+  end
+
+  def win_streak(game)
+    all = get_all_streaks(game)
+    all_win = streaks[:past].select {|s| s.win && !s.unbeaten }.sort_by {|s| s.resultTimes.size }.reverse
+    win = all_win.size > 0 && winStreaks[0] || Streak.new()
+
+    count = win.count
+
+    return {
+      'count': count,
+      'duration': duration
+    }
+  end
+
+end
+
+class Streak
+  attr_accessor :win
+  attr_accessor :unbeaten
+  attr_accessor :resultTimes
+
+  def initialize
+    @win = true
+    @unbeaten = false
+    @resultTimes = []
+  end
+
+  def count
+    return @resultTimes.size
+  end
+
+  def fromDate
+    return count > 0 && @resultTimes[0] || 0
+  end
+
+  def toDate
+    return count > 0 && @resultTimes[-1] || 0
   end
 end
