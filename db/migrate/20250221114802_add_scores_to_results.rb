f40c591354411a8ea3ed0ce99fc1218b19bcf605
James Muscat <james.muscat@alces-software.com>
2025-03-22 17:10:33 +0000

    Remove harmful/obsoleted migrations.

--- db/migrate/20250221114802_add_scores_to_results.rb
+++
@@ -1,6 +0,0 @@
-class AddScoresToResults < ActiveRecord::Migration[7.0]
-  def change
-    add_column :results, :for, :integer
-    add_column :results, :against, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_24_132401) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_085813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,16 +69,14 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_24_132401) do
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "for"
-    t.integer "against"
     t.index ["game_id"], name: "index_results_on_game_id"
   end
 
   create_table "teams", force: :cascade do |t|
+    t.integer "rank"
     t.bigint "result_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "winner"
     t.index ["result_id"], name: "index_teams_on_result_id"
   end
 
